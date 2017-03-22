114ea12b78a2fc4ad6f2c997620f46c3526d7ce8
Tommie <Tommie@Tommies-Mac.local>
2017-03-22 18:07:58 +0700

    Song 3 4 5

--- Another-Music-Player/ListViewController.swift
+++ Another-Music-Player/ListViewController.swift
@@ -27,11 +27,14 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
     
-    var SongList:[Song] = []
-    let song1:Song = Song(name: "Hello", artist: "Adele", lyrics: "Lyrics", cover: "Hello")
-    let song2:Song = Song(name: "ComeToMe", artist: "Avicii", lyrics: "Lyrics", cover: "ComeToMe")
-    let song3:Song = Song(name: "ColdWater", artist: "Justin Bieber", lyrics: "Lyrics", cover: "ColdWater")
+    let tmpLyrics = "Hello, It's me I was wondering if after all these years you'd like to meet to go over everything They say that time's supposed to heal you But I ain't done much healing \n Hello, can you hear me? I'm in California dreaming about who we used to be When we were younger and free I've forgotten how it felt before the world fell at our feet There's such a difference between us And a million miles \n\n Hello from the other side I must've called a thousand times to tell you I'm sorry, for everything that I've done But when I call you never seem to be home \n\n Hello from the outside At least I can say that I've tried to tell you I'm sorry, for breaking your heart But it don't matter, it clearly doesn't tear you apart anymore \n\n Hello, how are you? It's so typical of me to talk about myself I'm sorry, I hope that you're well Did you ever make it out of that town Where nothing ever happened? It's no secret That the both of us are running out of time \n\n Hello from the other side I must've called a thousand times to tell you I'm sorry, for everything that I've done But when I call you never seem to be home \n\n Hello from the outside At least I can say that I've tried to tell you I'm sorry, for breaking your heart But it don't matter, it clearly doesn't tear you apart anymore \n\n Ooooohh, anymore \n Ooooohh, anymore \n Ooooohh, anymore \n Anymore \n\n Hello from the other side I must've called a thousand times to tell you I'm sorry, for everything that I've done But when I call you never seem to be home \n\n Hello from the outside At least I can say that I've tried to tell you I'm sorry, for breaking your heart But it don't matter, it clearly doesn't tear you apart anymore"
     
+    var SongList:[Song] = []
+    let song1:Song = Song(name: "Hello", artist: "Adele", lyrics: tmpLyrics, cover: "Hello")
+    let song2:Song = Song(name: "ComeToMe", artist: "Avicii", lyrics: tmpLyrics, cover: "ComeToMe")
+    let song3:Song = Song(name: "ColdWater", artist: "Justin Bieber", lyrics: "Lyrics", cover: "ColdWater")
+    let song4:Song = Song(name: "WhereThemGirlsArt", artist: "David Guetta Feat Flo Rida & Nicki Minaj", lyrics: tmpLyrics, cover: "WhereThemGirlsAt")
+    let song5:Song = Song(name: "BlowYourMind", artist: "Dua Lipa", lyrics: tmpLyrics, cover:"BlowYourMind")
     
     
     override func viewDidLoad() {
@@ -41,6 +44,9 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         SongList.append(song1)
         SongList.append(song2)
+        SongList.append(song3)
+        SongList.append(song4)
+        SongList.append(song5)
      }
 
     override func didReceiveMemoryWarning() {
